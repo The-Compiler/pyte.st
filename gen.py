ec573795884fda6fe21665b9a274d6596c012856
Florian Bruhin <me@the-compiler.org>
2025-04-15 12:07:55 +0200

    Generate index.html

--- gen.py
+++ gen.py
@@ -1,10 +1,24 @@
 import pathlib
 
+import jinja2
+
+
 REPO_PATH = pathlib.Path(__file__).resolve().parent
 SYMLINKS = [
     ("htaccess", ".htaccess"),
     ("style.css", "style.css"),
 ]
+
+
+def gen_index() -> None:
+    template_path = REPO_PATH / "index.html.j2"
+    dest_path = REPO_PATH.parent / "index.html"
+
+    env = jinja2.Environment(autoescape=True)
+    template = env.from_string(template_path.read_text())
+
+    dest_path.write_text(template.render())
+    print("index.html generated.")
 
 
 def create_symlinks() -> None:
@@ -19,3 +33,4 @@ def create_symlinks() -> None:
 
 if __name__ == "__main__":
     create_symlinks()
+    gen_index()
