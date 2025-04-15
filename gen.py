2271b6a6dc0ee47eaa5f4ada344dea8e0a0c9928
Florian Bruhin <me@the-compiler.org>
2025-04-15 12:00:46 +0200

    Initial commit

---
+++ gen.py
@@ -0,0 +1,21 @@
+import pathlib
+
+REPO_PATH = pathlib.Path(__file__).parent
+SYMLINKS = [
+    ("htaccess", ".htaccess"),
+    ("style.css", "style.css"),
+]
+
+
+def create_symlinks() -> None:
+    for repo_name, dest_name in SYMLINKS:
+        repo_path = REPO_PATH / repo_name
+        dest_path = REPO_PATH.parent / dest_name
+        try:
+            dest_path.symlink_to(repo_path)
+        except FileExistsError:
+            print(f"{dest_path} already exists, skipping.")
+
+
+if __name__ == "__main__":
+    create_symlinks()
